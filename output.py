3db708a660f2cb87fc0f9f0b38f167d1ace75aa3
Maxim <82956250+Sullen-ui@users.noreply.github.com>
2022-08-27 21:03:40 +0700

    second commit

--- output.py
+++ output.py
@@ -8,16 +8,16 @@ html = """
     </table>
 """
 
-get_users = subprocess.run (["ps -Af | awk '{print $1}' | egrep -v UID | uniq -c"], stdout=subprocess.PIPE, shell=True)
+get_users = subprocess.run (["ps -Af | awk '{print $1}' | egrep -v UID | sort | uniq -c"], stdout=subprocess.PIPE, shell=True)
 s = str(get_users.stdout,'UTF-8')
 s = s.split()
+s.reverse()
 items = []
 for i in range(0,len(s),2):
-    items.append( s[i : 2 + i])   
-
+    items.append( s[i : 2 + i])
 for i in range(len(items)):
-    items[i][0] = int(items[i][0])
-    if items[i][0] > 10:
+    items[i][1] = int(items[i][1])
+    if items[i][1] > 10:
        items[i].append('&red')
     else:
        items[i].append('&green')    
@@ -28,5 +28,6 @@ f = open('table.html','w')
 f.write(html.format("".join(subitems)))
 f.close 
 
-get_users = subprocess.run (["telnet 192.168.200.3 1984 "status shift.user14 green '" + html + "'""], shell=True)
+# get_users = subprocess.run (['telnet 192.168.200.3 1984 "status shift.user14 green {0}"'.format(html)],shell=True)
+send = subprocess.run(['( echo "status shift.user14 green {0}"; sleep 1 ) | telnet 192.168.200.3 1984  2>&1 >/dev/null | grep -v "closed by foreign host"'.format(html.format("".join(subitems)))],shell=True)
 
